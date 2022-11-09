403cfe7b8538dab68c8d1d4239fa2e44f103157c
Marwa-MEZLINI <m.mezlini92@gmail.com>
2022-11-09 11:02:08 +0100

    onclick check mark done

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 //class toggle to add a check mark on click
 
-function treated(element) {
-    let elem = document.querySelector(`#${element.id} > .rows .row_1 .not-treated`)
+function classToggle(element) {
+    let elem = document.querySelector(`#${element.id} > .rows > .row_1`).lastElementChild;
     elem.classList.toggle("treated")
 }
 
-console.log(treated(element))
+function treated(element) {
+    classToggle(element)
+}
+
